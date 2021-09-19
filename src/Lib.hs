f1faf0421663043a1910876abafe69b7f46cd968
Ивашков Дмитрий Александрович <100100101104@mail.ru>
2021-09-20 02:58:20 +0300

    simple QuadTree

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,11 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+type Data = Float
+
+data QuadTree = Quad QuadTree QuadTree QuadTree QuadTree Int | Unique Data Int deriving(Show)
+
+size :: QuadTree -> Int
+size (Quad _ _ _ _ int) = int
+size (Unique _ int) = int
