e85ce7adbc476eeaa0e3d997ddf4118c82d6cd8d
Ивашков Дмитрий Александрович <100100101104@mail.ru>
2021-09-20 17:48:46 +0300

    simple Main

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,10 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  print m
+  print $ m *** m
+    where
+      m = concatMatrix one one zero one
+      one = ones 1
+      zero = zeros 1
