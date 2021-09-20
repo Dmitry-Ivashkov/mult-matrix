335ae4103226750af2cff05e333cf2c2d332fd28
Ивашков Дмитрий Александрович <100100101104@mail.ru>
2021-09-20 16:10:19 +0300

    add Eq

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,6 +13,15 @@ type Data = Int
 --q4 q3
 data QuadTree = Quad QuadTree QuadTree QuadTree QuadTree | Unique Data Int deriving(Show)
 
+instance Eq QuadTree where
+  (Unique d1 s1) == (Unique d2 s2) = (s1 == s2) && (d1 == d2)
+  (Quad q1_1 q1_2 q1_3 q1_4) == (Quad q2_1 q2_2 q2_3 q2_4) = (q1_1 == q2_1) && (q1_2 == q2_2) && (q1_3 == q2_3) && (q1_4 == q2_4)
+  (Quad q1 q2 q3 q4) == (Unique d2 s) = (q1 == u) && (q2 == u) && (q3 == u) && (q4 == u) where
+    u = Unique d2 (divInt s 2)
+  q1 == q2 = q2 == q1
+
+-- TODO(Show)
+
 size :: QuadTree -> Int
 size (Quad q _ _ _) = 2 * (size q)
 size (Unique _ int) = int
@@ -33,6 +42,6 @@ infixl 6  +++
 (+++) :: QuadTree -> QuadTree -> QuadTree
 (Unique d1 s) +++ (Unique d2 _) = Unique (d1 + d2) s
 (Quad q1 q2 q3 q4) +++ (Unique d2 s) = (Quad (q1 +++ u) (q2 +++ u) (q3 +++ u) (q4 +++ u)) where
-  u = Unique d2 (divInt s2 2)
+  u = Unique d2 (divInt s 2)
 q1@(Unique _ _) +++ q2 = q2 +++ q1
 (Quad q1_1 q1_2 q1_3 q1_4) +++ (Quad q2_1 q2_2 q2_3 q2_4) = (Quad (q1_1 +++ q2_1) (q1_2 +++ q2_2) (q1_3 +++ q2_3) (q1_4 +++ q2_4))
