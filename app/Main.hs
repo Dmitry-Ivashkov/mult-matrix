bb3f2186d2832e453d1b2acea1f643275d14c14f
Ивашков Дмитрий Александрович <100100101104@mail.ru>
2021-11-10 18:22:48 +0300

    simple main

--- app/Main.hs
+++ app/Main.hs
@@ -4,9 +4,7 @@ import Lib
 
 main :: IO ()
 main = do
-  print m
-  print $ m *** m
+  print $ (multiply (*) (+) 2 3 m1 m1) == (mCreateSimpleMatrix 3 2 2) -- dim = 3, size = 2, matrix = [[[2,2],[2,2]],[[2,2],[2,2]]]
     where
-      m = concatMatrix one one zero one
-      one = ones 1
-      zero = zeros 1
+      m1 = mConcatMatrix $ replicate 8 one -- dim = 3, size = 2, matrix = [[[1,1],[1,1]],[[1,1],[1,1]]]
+      one = mOnes 3 1 -- dim = 3, size = 1, matrix = [[[1]]]
